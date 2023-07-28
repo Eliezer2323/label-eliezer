17d3d6e11957e47010e40a27dd3598ab6510e982
Eliezer Belloso <Eliezer Belloso@PC-ELIEZER>
2023-07-28 10:57:31 -0600

    Agregar archivos de proyecto.

---
+++ label eliezer/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace label_eliezer
+{
+           public partial class MainPage : ContentPage
+           {
+                /// <summary>
+                /// Método constructor que se inicia al abrir la app
+                /// </summary>
+                public MainPage()
+                {
+                    InitializeComponent();
+                }
+           }
+ }
+
+
+
+
